9d1da84d98867e445d7f9f33b3f188b0ecba2a97
sebprt <sebastien.parrat@orange.fr>
2021-05-28 10:53:26 +0200

    Added Isolated Builders

---
+++ src/Builder/IsolatedValueAppendingBuilder.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Kiboko\Component\SatelliteToolbox\Builder;
+
+use PhpParser\Builder;
+use PhpParser\Node;
+
+final class IsolatedValueAppendingBuilder implements Builder
+{
+    /** @var Node\Expr[] */
+    private array $usedVariables;
+
+    public function __construct(
+        private Node\Expr $input,
+        private Node\Expr $output,
+        private array $stmts,
+        Node\Expr ...$usedVariables
+    )
+    {
+        $this->usedVariables = $usedVariables;
+    }
+
+    public function getNode(): Node
+    {
+        return new Node\Expr\Assign(
+            $this->output,
+            new Node\Expr\FuncCall(
+                new Node\Expr\Closure([
+                    'params' => [
+                        new Node\Param(
+                            var: new Node\Expr\Variable('input'),
+                        )
+                    ],
+                    'stmts' => $this->stmts,
+                    'uses' => [
+                        ...$this->usedVariables,
+                        new Node\Expr\Variable('output')
+                    ]
+                ]),
+                [
+                    new Node\Arg($this->input),
+                ]
+            )
+        );
+    }
+}
