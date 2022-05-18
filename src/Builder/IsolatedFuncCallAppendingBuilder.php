1642642e2c0fad1e0390d9bb1b141029e7aaccc7
sebprt <sebastien@kiboko.fr>
2022-05-18 10:00:32 +0200

    Added a new builder that produces a func call node

---
+++ src/Builder/IsolatedFuncCallAppendingBuilder.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Kiboko\Component\SatelliteToolbox\Builder;
+
+use PhpParser\Builder;
+use PhpParser\Node;
+
+final class IsolatedFuncCallAppendingBuilder implements Builder
+{
+    /** @var Node\Expr[] */
+    private array $usedVariables;
+
+    public function __construct(
+        private Node\Expr $input,
+        private array $stmts,
+        Node\Expr ...$usedVariables
+    ) {
+        $this->usedVariables = $usedVariables;
+    }
+
+    public function getNode(): Node
+    {
+        return new Node\Stmt\Expression(
+            new Node\Expr\FuncCall(
+                new Node\Expr\Closure([
+                    'params' => [
+                        new Node\Param(
+                            var: new Node\Expr\Variable('input'),
+                        )
+                    ],
+                    'stmts' => $this->stmts,
+                    'uses' => [
+                        ...$this->usedVariables,
+                        new Node\Expr\Variable('output')
+                    ]
+                ]),
+                [
+                    new Node\Arg($this->input),
+                ]
+            ),
+        );
+    }
+}
